047c17933befb8559dbaea4b237584c7c2cfce8b
Ashwin Pillai <ashwin96@bu.edu>
2023-02-03 03:11:52 -0500

    update credential feature

---
+++ src/main/java/com/projects/spring/cloudstorage/mappers/CredentialMapper.java
@@ -0,0 +1,28 @@
+package com.projects.spring.cloudstorage.mappers;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.projects.spring.cloudstorage.models.Credential;
+
+@Mapper
+public interface CredentialMapper {
+
+    @Select("SELECT * FROM CREDENTIALS WHERE credentialId = #{id}")
+    Credential findCredentialById(int id);
+
+    @Update("UPDATE CREDENTIALS SET url = #{url}, username = #{username}, key = #{key}, password = #{password} WHERE userId = #{userId} AND credentialId = #{credentialId}")
+    int updateCredential(Credential credential);
+
+    @Insert("INSERT INTO CREDENTIALS (url, username, key, password, userId) VALUES (#{url}, #{username}, #{key}, #{password}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "credentialId")
+    int createCredential(Credential credential);
+
+    @Delete("DELETE FROM CREDENTIALS WHERE credentialId = #{credentialId}")
+    int deleteCredentialById(int credentialId);
+
+}
--- src/main/java/com/projects/spring/cloudstorage/services/CredentialService.java
+++ src/main/java/com/projects/spring/cloudstorage/services/CredentialService.java
@@ -1,30 +1,70 @@
 package com.projects.spring.cloudstorage.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.projects.spring.cloudstorage.mappers.CredentialMapper;
 import com.projects.spring.cloudstorage.models.Credential;
 
 @Service
 public class CredentialService {
+    @Autowired
+    private CredentialMapper credMapper;
+    @Autowired
+    private EncryptionService encryptionService;
 
     public boolean updateCredential(Credential credential) {
+        Credential existingCredential = credMapper.findCredentialById(credential.getCredentialId());
+
+        if(existingCredential != null) {
+            String decryptedPassword = encryptionService.decryptValue(existingCredential.getPassword(), existingCredential.getKey());
+            if(!(credential.getPassword().equals(decryptedPassword))) {
+                String newKey = encryptionService.generateKey();
+                String newPassword = encryptionService.encryptValue(credential.getPassword(), newKey);
+                existingCredential.setKey(newKey);
+                existingCredential.setPassword(newPassword);
+            }
+            if(credential.getUrl() != null && !credential.getUrl().equals(existingCredential.getUrl())) {
+                existingCredential.setUrl(credential.getUrl());
+            }
+            if(!credential.getUsername().isEmpty() && !credential.getUsername().equals(existingCredential.getUsername())) {
+                existingCredential.setUsername(credential.getUsername());
+            }
+            return credMapper.updateCredential(existingCredential) > 0;
+        }
         return false;
     }
 
     public boolean addCredential(Credential credential) {
-        return false;
+        String newKey = encryptionService.generateKey();
+        String newPassword = encryptionService.encryptValue(credential.getPassword(), newKey);
+        credential.setKey(newKey);
+        credential.setPassword(newPassword);
+        int created = credMapper.createCredential(credential);
+        return created > 0;
     }
 
     public boolean deleteCredential(int credentialId) {
+        if(credentialExists(credentialId)) {
+            return credMapper.deleteCredentialById(credentialId) > 0;
+        }
         return false;
     }
 
+    private boolean credentialExists(int credentialId) {
+        return credMapper.findCredentialById(credentialId) != null;
+    }
+
     public Credential getCredential(int credentialId) {
-        return null;
+        Credential credentialById = credMapper.findCredentialById(credentialId);
+        return credentialById;
     }
 
     public String decryptPassword(Credential credential) {
-        return null;
+        if(credential.getCredentialId() == null || credential.getKey() == null) {
+            return "";
+        }
+        return encryptionService.decryptValue(credential.getPassword(), credential.getKey());
     }
     
 }
